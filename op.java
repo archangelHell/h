bd7510764177fff05c0df2c3ee6852836a8e13a2
archangelHell <118906144+archangelHell@users.noreply.github.com>
2023-08-08 21:37:20 +0300

    Add files via upload

---
+++ op.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class op {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write number:");
+        int a = sc.nextInt();
+        System.out.println("u write" + a);
+    }
+}
